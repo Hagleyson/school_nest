37aeb183d03bf2029ce00848e180bbf0c9a318de
“hagleyson” <hagleyson@gmail.com>
2023-04-25 14:01:16 -0300

    ♻️ adjustment test for delete students

--- src/application/use-cases/students/delete-students.spec.ts
+++ src/application/use-cases/students/delete-students.spec.ts
@@ -4,13 +4,13 @@ import { InMemoryStudentRepository } from '../../../test/repositories/in-memory-
 import { CreateStudent } from './create-students';
 import { ListStudent } from './list-students';
 import { DeleteStudents } from './delete-students';
+import { StudentNotFound } from './erros/student-not-found';
 
 describe('Delete student use cases', () => {
+  const studentRepository = new InMemoryStudentRepository();
+  const deleteStudent = new DeleteStudents(studentRepository);
   it('should be able to delete student', async () => {
-    const studentRepository = new InMemoryStudentRepository();
-
     const createStudent = new CreateStudent(studentRepository);
-    const deleteStudent = new DeleteStudents(studentRepository);
     const listStudent = new ListStudent(studentRepository);
 
     const course = makeCourse();
@@ -28,5 +28,10 @@ describe('Delete student use cases', () => {
     const { student: list } = await listStudent.execute();
 
     expect(list).toHaveLength(0);
+  });
+  it('should not be able to delete the student', async () => {
+    expect(async () => {
+      return deleteStudent.execute({ id: 8 });
+    }).rejects.toThrow(StudentNotFound);
   });
 });
